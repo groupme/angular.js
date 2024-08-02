800b59a5a5b72aa63378ebbf83c964b2fa9a4f6c
Jiatang Dong <jiadon@microsoft.com>
2024-08-02 13:11:18 -0700

    fix a eslint problem

--- src/Angular.js
+++ src/Angular.js
@@ -1805,7 +1805,7 @@ function bootstrap(element, modules, config) {
       throw ngMinErr(
           'btstrpd',
           'App already bootstrapped with this element \'{0}\'',
-          tag.replace(/</g, '&lt;').replace(/>/g, '&gt;')); // Added 'g' flag to replace all 
+          tag.replace(/</g, '&lt;').replace(/>/g, '&gt;'));// Added 'g' flag to replace all
     }
 
     modules = modules || [];
